b205fe1f3e9a5311f8bd264763f2b025ed37a541
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2023-05-31 15:37:44 +0600

    frontend file added by menual process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
-import { addToDo, getAllToDo } from "./utils/HandleApp";
+import { addToDo, deleteToDo, getAllToDo , updateToDo} from "./utils/HandleApp";
 
 
 function App() {
 
 
 const [toDo, setTodo] = useState([])
-const [text, setText] = useState("hi")
+const [text, setText] = useState("")
+const [isUpdate, setIsUpdate] = useState(false)
+const [toDoId, setToDoId] = useState("")
+
 useEffect(()=>{
   getAllToDo(setTodo)
 },[])
+
+const updateModefun = (_id, text) => {
+  setIsUpdate(true)
+  setText(text)
+  setToDoId(_id)
+}
+
+
 
   return (
     <div className="App">
@@ -26,14 +37,22 @@ useEffect(()=>{
           onChange={(e)=> setText(e.target.value)}
           ></input>
 
-          <div className="add" onClick={addToDo}>
-            Add
+          <div className="add" onClick={()=>
+             isUpdate? 
+             updateToDo(toDoId, text, setText, setTodo, setIsUpdate) 
+             : addToDo(text, setText, setTodo)
+             }>
+            {isUpdate ? "Update": "Add"}
           </div>
         </div>
-        {/* <div>${text}</div> */}
         <div className="list">
           
-           {toDo.map((item)=> <ToDo key={item._id} text={item.text}/>)}
+           {toDo.map((item)=>
+            <ToDo key={item._id}
+             text={item.text}
+             updateMode={()=> updateModefun(item._id, item.text)} 
+             deleteToDo={() => deleteToDo(item._id, setTodo)}
+             />)}
         </div>
       </div>
     </div>
--- frontend/src/utils/HandleApp.js
+++ frontend/src/utils/HandleApp.js
@@ -6,7 +6,7 @@ const getAllToDo = (setTodo) =>{
     axios
     .get(baseUrl)
     .then((data)=>{
-        console.log( 'data......>' );
+        console.log( 'data......>', data );
         setTodo(data.data)
     })
 
@@ -14,8 +14,36 @@ const getAllToDo = (setTodo) =>{
 
 const addToDo = (text, setText, setToDo) =>{
 
+    axios
+    .post(`${baseUrl}/add`,{text})
+    .then((data)=>{
+        console.log(data.data);
+        setText("")
+        getAllToDo(setToDo)
+    }).catch((err)=> console.log(err))
+}
+
+const updateToDo = (toDoId, text, setText, setToDo, setIsUpdate) =>{
+
+    axios
+    .post(`${baseUrl}/update`,{_id: toDoId, text})
+    .then((data)=>{
+        console.log(data.data);
+        setText("")
+        setIsUpdate(false)
+        getAllToDo(setToDo)
+    }).catch((err)=> console.log(err))
+}
+
+const deleteToDo = (_id, setToDo)=>{
+    axios
+    .post(`${baseUrl}/delete`,{_id})
+    .then((data)=>{
+        console.log(data.data);
+        getAllToDo(setToDo)
+    }).catch((err)=> console.log(err))
 
 }
 
-export {getAllToDo, addToDo}
+export {getAllToDo, addToDo, updateToDo, deleteToDo}
 
